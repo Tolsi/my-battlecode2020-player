32cc5000ae107e8f5ead351c05e3d20c16ba3e77
Sergey Tolmachev <tolsi.ru@gmail.com>
2020-01-20 02:40:02 +0530

    better wall building

--- LLandscaperDigHQWallTask.java
+++ LLandscaperDigHQWallTask.java
@@ -28,30 +28,35 @@ public class LLandscaperDigHQWallTask extends LTask {
 
     static boolean place = false;
 
-
     @Override
     void step() throws GameActionException {
         if (SState.hqLoc != null && GS.c.isReady()) {
-            for (Direction dir : UDirections.all) {
-                MapLocation tileToCheck = GS.c.getLocation().add(dir);
-                int distanceToHq = tileToCheck.distanceSquaredTo(SState.hqLoc);
-                if (GS.c.canSenseLocation(tileToCheck) && distanceToHq < 4 && distanceToHq > 0 && GS.c.senseElevation(tileToCheck) < lowestElevation) {
-                    lowestElevation = GS.c.senseElevation(tileToCheck);
+            for (Direction dir : UDirections.withoutCenter) {
+                MapLocation tileToCheck = SState.hqLoc.add(dir);
+                int elevation;
+                if (GS.c.canSenseLocation(tileToCheck) && (elevation = GS.c.senseElevation(tileToCheck)) < lowestElevation) {
+                    lowestElevation = elevation;
                     bestPlaceToBuildWall = tileToCheck;
                 }
             }
-
-            if (GS.c.getDirtCarrying() < RobotType.LANDSCAPER.dirtLimit && !place) {
-                findGoodPlaceAndDigAroundHQ();
-            } else if (bestPlaceToBuildWall != null &&
-                    GS.c.getLocation().distanceSquaredTo(bestPlaceToBuildWall) < 4 &&
-                    GS.c.canDepositDirt(GS.c.getLocation().directionTo(bestPlaceToBuildWall)) &&
-                    (place || GS.c.getDirtCarrying() == RobotType.LANDSCAPER.dirtLimit)) {
-                GS.c.depositDirt(GS.c.getLocation().directionTo(bestPlaceToBuildWall));
+            if (GS.c.getLocation().distanceSquaredTo(bestPlaceToBuildWall) >= 2 &&
+                    GS.goTo(GS.c.getLocation().directionTo(bestPlaceToBuildWall))) {
+                System.out.println("Going to better point to build");
+            } else if (GS.c.getDirtCarrying() < RobotType.LANDSCAPER.dirtLimit && !place) {
+                if (findGoodPlaceAndDigAroundHQ()) {
+                    System.out.println("Digging...");
+                }
+            } else if (bestPlaceToBuildWall != null && (place || GS.c.getDirtCarrying() == RobotType.LANDSCAPER.dirtLimit)) {
+                if (GS.c.getLocation().distanceSquaredTo(bestPlaceToBuildWall) < 4 && GS.c.canDepositDirt(GS.c.getLocation().directionTo(bestPlaceToBuildWall))) {
+                    System.out.printf("building a wall to %s\n", bestPlaceToBuildWall);
+                    GS.c.depositDirt(GS.c.getLocation().directionTo(bestPlaceToBuildWall));
+                } else if (GS.c.canDepositDirt(Direction.CENTER)) {
+                    System.out.printf("building a wall to %s\n", GS.c.getLocation());
+                    GS.c.depositDirt(Direction.CENTER);
+                }
+                place = GS.c.getDirtCarrying() > 0;
                 lowestElevation = Integer.MAX_VALUE;
                 bestPlaceToBuildWall = null;
-                System.out.println("building a wall");
-                place = GS.c.getDirtCarrying() > 0;
             }
         }
     }
